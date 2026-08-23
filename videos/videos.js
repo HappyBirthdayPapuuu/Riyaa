a59a5de5ca31153bd81edba346291c0c04162db0
HarrishArun <ha1756@srmist.edu.in>
2026-08-23 23:02:32 +0530

    Add birthday video messages and sync video catalog

--- videos/videos.js
+++ videos/videos.js
@@ -1,3 +1,16 @@
 // Auto-generated video list from videos/ folder.
 // Drop any new videos in videos/ and run python3 sync_all.py to refresh.
-window.TREASURE_VIDEOS = [];
+window.TREASURE_VIDEOS = [
+  {
+    "file": "Hemi.mp4",
+    "caption": "from Hemi"
+  },
+  {
+    "file": "Hrudaya.mp4",
+    "caption": "from Hrudaya"
+  },
+  {
+    "file": "Sheetha.mp4",
+    "caption": "from Sheetha"
+  }
+];
